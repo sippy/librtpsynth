4145391b30eaff91c4ea8613a7fcbddddf4b9e90
Maksym Sobolyev <sobomax@sippysoft.com>
2023-10-17 12:36:44 -0700

    Eliminate compiler warnings (signed vs unsigned/void* vs char*),

--- src/rtpsynth.h
+++ src/rtpsynth.h
@@ -34,7 +34,7 @@ struct rsynth_seq {
 
 void *rsynth_ctor(int srate, int ptime);
 void *rsynth_next_pkt(void *ri, int plen, int pt);
-int rsynth_next_pkt_pa(void *ri, int plen, int pt, void *buf, unsigned int blen, int pa);
+int rsynth_next_pkt_pa(void *ri, int plen, int pt, char *buf, unsigned int blen, int pa);
 void rsynth_pkt_free(void *rnp);
 void rsynth_dtor(void *ri);
 unsigned int rsynth_set_mbt(void *ri, unsigned int new_st);
