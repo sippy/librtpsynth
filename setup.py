8f3c9170177bf648f1061ddf10ef3a81b1d91f76
Maksym Sobolyev <sobomax@sippysoft.com>
2026-02-09 16:01:59 -0800

    Make this 1.3.0.

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 kwargs = {'name':'rtpsynth',
-      'version':'1.2.1',
+      'version':'1.3.0',
       'description':'Library optimized to generate/process sequence of the RTP packets',
       'long_description': long_description,
       'long_description_content_type': "text/markdown",
